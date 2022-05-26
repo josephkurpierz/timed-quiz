bfec7caff9f84007ad6423424a7bf97e84c03ce5
Joseph Kurpierz <josephkurpierz@gmail.com>
2022-05-26 18:56:43 -0500

    not a whole lot

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,19 @@
-var startButtonEl = document.querySelector(".start-btn");
-var questionBoxEl = document.querySelector(".question-box")
-var choiceButtonEl = document.querySelector(".choice-btn");
-var questionEl = document.querySelector("#question");
-var timeEl = document.querySelector("#time");
-var frontPageEl = document.querySelector(".front-page");
+var startButtonEl = document.getElementById("start-btn");
+var questionBoxEl = document.getElementById("question-box")
+var choiceButtonEl = document.getElementById("choice-btn");
+var questionEl = document.getElementById("question");
+var timeEl = document.getElementById("time");
+var frontPageEl = document.getElementById("front-page");
 var currentQuestion = 0;
-var choiceBoxEl = document.querySelector(".choices");
+var choiceBoxEl = document.getElementById("choices");
 var timerCount = 60;
-var gradeEl = document.querySelector(".grade");
+var gradeEl = document.getElementById("grade");
 
 //question array to pull questions from
-var questionArrEl =[
+var questionArrEl = [
   {
     question: "how many ducks go quack?",
-    choices: ["1","2","3","4"],
+    choices: ["1", "2", "3", "4"],
     correct: 1,
   },
   {
@@ -23,75 +23,87 @@ var questionArrEl =[
   },
   {
     question: "how helpful are the classes in bootcamp?",
-    choices: ["a little" , "none", "like bubbles in space", "more confusing than anything"],
+    choices: ["a little", "none", "like bubbles in space", "more confusing than anything"],
     correct: 3,
   },
   {
     question: "What does HTML stand for?",
-    choices:["hypertext markup language", "cascading stylesheets", "I like turtles", "programmers in the wild"],
+    choices: ["hypertext markup language", "cascading stylesheets", "I like turtles", "programmers in the wild"],
     correct: 1
   }
 ]
 
 // front page card
-var startButtonHandler = function(){
+var startButtonHandler = function () {
   frontPageEl.className = "hide";
   startTimer();
   startQuiz();
 };
 
-var startTimer = function(){
-  var timeInterval = setInterval(function(){
-    if (timerCount>=0){
-      timeEl.textContent=timerCount;
+var startTimer = function () {
+  var timeInterval = setInterval(function () {
+    if (timerCount >= 0) {
+      timeEl.textContent = timerCount;
       timerCount--;
     } else
-    clearInterval(timeInterval);
-  },1000);
+      clearInterval(timeInterval);
+  }, 1000);
 };
 
 //initiate quiz by pulling questions and choices from arrays
-var startQuiz = function(){
-  // for ( var i =0; i<questionArrEl.length; i++){
-  renderQuestion(currentQuestion);//change for dynamics
+var startQuiz = function () {
+  renderQuestion(currentQuestion);
   renderChoices(currentQuestion);
-  currentQuestion++;
-  // };
 }
 
-var renderQuestion = function(currentQuestion){
-  questionEl.innerHTML="<h1 id='question' class='question'>"+ questionArrEl[currentQuestion].question+"</h1>";
+//load current question into questionbox
+var renderQuestion = function (currentQuestion) {
+  questionEl.innerHTML = "<h1 id='question' class='question'>" + questionArrEl[currentQuestion].question + "</h1>";
   questionBoxEl.appendChild(questionEl);
 };
 
-var renderChoices = function (currentQuestion){
-  for (var i =0; i<questionArrEl.length; i++){
+//load choice buttons into choices
+var renderChoices = function (currentQuestion) {
+  for (var i = 0; i < questionArrEl.length; i++) {
+    
     choiceButtonEl = document.createElement("button");
-    choiceButtonEl.innerHTML = "<div class='choices'><div class='choice'><button class='btn choice-btn' id="+ i +">"+questionArrEl[currentQuestion].choices[i];+"</button></div></div>";
+    choiceButtonEl.innerHTML = "<button class='btn choice-btn' id='choice-btn' data-num='i'>" + questionArrEl[currentQuestion].choices[i]; +"</button>";
     choiceBoxEl.appendChild(choiceButtonEl);
   };
 }
 
-var answerHandler = function(event){
-  var targetEl = event.target;
-  var choice = targetEl.getAttribute("data-num");
-  console.log(choice);
-  if(choice === questionArrEl[currentQuestion].correct){
-    gradeEl = document.createElement("div");
-    //display correct
-  } else{
-    timerCount=timerCount-10;
-    //display wrong
+var answerHandler = function () {
+
+  
+  var dataNum = choiceButtonEl.getAttribute("data-num");
+console.log(dataNum);
+  gradeEl = document.createElement("div");
+  if (dataNum === questionArrEl[currentQuestion].correct) {
+    gradeEl.innerHTML = "<div id='grade' class='grade'>Correct!</div>";
+  } else {
+    timerCount = timerCount - 10;
+    gradeEl.textContent = "Wrong!";
   }
   currentQuestion++
-  if(currentQuestion < questionArrEl.length){
+  hidePrevious();
+  if (currentQuestion < questionArrEl.length) {
     renderQuestion(currentQuestion);
     renderChoices(currentQuestion);
-  } else{
-    var currentScore=timerCount;
+  } else {
+    var currentScore = timerCount;
+
     //go to complete page
   }
   // var selectedChoice = choiceButtonEl.getElementBy
+}
+
+var hidePrevious = function(){
+  for (let i = 0; i < questionArrEl.length; i++) {
+    // middle fingers
+    
+    
+  }
+
 }
 // question card
 // start timer
@@ -113,5 +125,5 @@ var answerHandler = function(event){
 // retrieve high scores from local storage
 
 
-startButtonEl.addEventListener("click",startButtonHandler);
-choiceButtonEl.addEventListener("click", answerHandler);
+startButtonEl.addEventListener("click", startButtonHandler);
+choiceBoxEl.addEventListener("click", answerHandler);
