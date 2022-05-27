1bd02a9caea709915c4c4ac926588c4ffdf6acc4
Joseph Kurpierz <josephkurpierz@gmail.com>
2022-05-26 23:33:59 -0500

    fixed timer display, got choices to render properly, load endgame, store score and initials

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,30 +8,35 @@ var currentQuestion = 0;
 var choiceBoxEl = document.getElementById("choices");
 var timerCount = 60;
 var gradeEl = document.getElementById("grade");
+var currentScore = 0;
+var inputEl = document.getElementById("initials");
+var submitBtnEl = document.getElementById("submit-btn");
+var scoreEl = document.getElementById("score");
+
 
 //question array to pull questions from
 var questionArrEl = [
   {
     question: "how many ducks go quack?",
     choices: ["1", "2", "3", "4"],
-    correct: 1,
+    correct: "1",
   },
   {
     question: "A very useful tool used during development and debugging for printing content to the debugger is:",
     choices: ["devTools", "a friend", "gitbash", "github"],
-    correct: 1,
+    correct: "devTools",
   },
   {
     question: "how helpful are the classes in bootcamp?",
     choices: ["a little", "none", "like bubbles in space", "more confusing than anything"],
-    correct: 3,
+    correct: "like bubbles in space",
   },
   {
     question: "What does HTML stand for?",
     choices: ["hypertext markup language", "cascading stylesheets", "I like turtles", "programmers in the wild"],
-    correct: 1
+    correct: "hypertext markup language"
   }
-]
+];
 
 // front page card
 var startButtonHandler = function () {
@@ -39,9 +44,9 @@ var startButtonHandler = function () {
   startTimer();
   startQuiz();
 };
-
+var timeInterval=0;
 var startTimer = function () {
-  var timeInterval = setInterval(function () {
+  timeInterval = setInterval(function () {
     if (timerCount >= 0) {
       timeEl.textContent = timerCount;
       timerCount--;
@@ -58,31 +63,39 @@ var startQuiz = function () {
 
 //load current question into questionbox
 var renderQuestion = function (currentQuestion) {
-  questionEl.innerHTML = "<h1 id='question' class='question'>" + questionArrEl[currentQuestion].question + "</h1>";
+  questionEl=document.createElement("h1");
+  questionEl.className="question";
+  questionEl.textContent= questionArrEl[currentQuestion].question;
   questionBoxEl.appendChild(questionEl);
 };
 
 //load choice buttons into choices
 var renderChoices = function (currentQuestion) {
   for (var i = 0; i < questionArrEl.length; i++) {
-    
     choiceButtonEl = document.createElement("button");
-    choiceButtonEl.innerHTML = "<button class='btn choice-btn' id='choice-btn' data-num='i'>" + questionArrEl[currentQuestion].choices[i]; +"</button>";
+    choiceButtonEl.className = "choice-btn btn";
+    choiceButtonEl.textContent= questionArrEl[currentQuestion].choices[i];
     choiceBoxEl.appendChild(choiceButtonEl);
   };
 }
 
-var answerHandler = function () {
-
-  
-  var dataNum = choiceButtonEl.getAttribute("data-num");
-console.log(dataNum);
+// after first question is answered, runs through other questions
+// subtracting time for incorrect choices
+var answerHandler = function (event) {
+  var targetEl = event.target;
+  var choiceIndex = targetEl.outerText;
+  console.log("this is choiceIndex" + choiceIndex);
+  console.log("this is correct answer" +questionArrEl[currentQuestion].correct);
   gradeEl = document.createElement("div");
-  if (dataNum === questionArrEl[currentQuestion].correct) {
-    gradeEl.innerHTML = "<div id='grade' class='grade'>Correct!</div>";
+  gradeEl.className = "grade"
+  if (choiceIndex === questionArrEl[currentQuestion].correct) {
+    //gradeEl.textContent="Correct!";
+    //questionBoxEl.appendChild(gradeEl);
+    // wanted to 'questionBoxEl.removeChild(gradeEl);' after an interval but ran out of time
   } else {
     timerCount = timerCount - 10;
-    gradeEl.textContent = "Wrong!";
+    //gradeEl.textContent="Wrong!";
+    //questionBoxEl.appendChild(gradeEl);
   }
   currentQuestion++
   hidePrevious();
@@ -90,20 +103,46 @@ console.log(dataNum);
     renderQuestion(currentQuestion);
     renderChoices(currentQuestion);
   } else {
-    var currentScore = timerCount;
+    clearInterval(timeInterval);
+    currentScore = timerCount;
+    timeEl.textContent = currentScore;
+    renderCompletePage();
+  };
+};
 
-    //go to complete page
-  }
-  // var selectedChoice = choiceButtonEl.getElementBy
+//display the endgame page with player score
+var renderCompletePage = function(){
+  var completedPageEl = document.getElementById("completed");
+  completedPageEl.classList.remove("hide");
+  scoreEl.textContent = currentScore;
+};
+
+//remove last question and choice boxes
+var hidePrevious = function () {
+  while (choiceBoxEl.lastChild) {
+    choiceBoxEl.removeChild(choiceBoxEl.lastChild);
+  };
+  questionBoxEl.removeChild(questionEl);
+};
+
+var scoreStorageArr=[];
+//submit button to store high score
+var endgame = function(){
+  scoreStorageArr =[
+    {
+      initials: inputEl.value,
+      score: currentScore,
+    }
+  ];
+ 
+  localStorage.setItem("highScores", JSON.stringify(scoreStorageArr));
+  open("./high-score.html");
+  viewHighScores();
 }
 
-var hidePrevious = function(){
-  for (let i = 0; i < questionArrEl.length; i++) {
-    // middle fingers
-    
-    
-  }
-
+var viewHighScores = function(){
+  scoreStorage = JSON.parse(localStorage.getItem("highScores"));
+  //high score display highScoresArr
 }
 // question card
 // start timer
@@ -127,3 +166,4 @@ var hidePrevious = function(){
 
 startButtonEl.addEventListener("click", startButtonHandler);
 choiceBoxEl.addEventListener("click", answerHandler);
+submitBtnEl.addEventListener("click", endgame);
